baf7182fdb4be6aba2fa5579f6c52b046c496c4d
Mubdi Rahman <mubdi@sidratresearch.com>
2024-05-13 18:05:31 -0400

    Started framing HTML

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path'
+
+export default {
+    plugins: [handlebars({
+        partialDirectory: resolve(__dirname, 'partials')
+    })],
+};
