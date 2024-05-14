1c33d149d89eed8fdf7d663ace560f3c4291c319
Mubdi Rahman <mubdi@sidratresearch.com>
2024-05-14 17:43:09 -0400

    Have dragging working

--- js/dragging.js
+++ js/dragging.js
@@ -4,17 +4,26 @@ function dragElement(elmnt) {
 
     let pos1 = 0, pos2 = 0, pos3 = 0, pos4;
 
+    let draggingHandle
+
     if (document.getElementById(elmnt.id + "-bar")) {
         /* if present, the bar is where you move the DIV from:*/
 
-        document.getElementById(elmnt.id + "-bar").addEventListener('mousedown', dragMouseDown);
+        draggingHandle = document.getElementById(elmnt.id + "-bar")
+
     } else {
         /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elmnt.addEventListener('mousedown', dragMouseDown);
+        draggingHandle = elmnt
     }
+
+    draggingHandle.addEventListener('mousedown', dragMouseDown);
+
 
     function dragMouseDown(e) {
         e.preventDefault();
+
+        e.target.style.cursor = "grabbing";
+
         // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
@@ -35,7 +44,8 @@ function dragElement(elmnt) {
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
     }
 
-    function closeDragElement() {
+    function closeDragElement(e) {
+        e.target.style.cursor = "grab";
         // stop moving when mouse button is released:
         document.removeEventListener('mouseup', closeDragElement)
         document.removeEventListener('mousemove', elementDrag)
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as d3 from 'd3';
 import * as PIXI from 'pixi.js';
 import { Ease, ease } from 'pixi-ease';
 import { addDraggingToElements } from './js/dragging.js'
+import { startUIInteractions } from './js/uiinteractions.js';
 
 import './style/style.scss'
 
@@ -11,31 +12,9 @@ let app_length = document.getElementById("app").clientWidth;
 let app_height = document.getElementById("app").clientHeight;
 
 
+// Setup UI Interactions
+startUIInteractions();
 
-// Detail Panel 
-
-function closeDetailPanel() {
-  // Function to Close the details panel:
-
-  const detailsPanel = document.getElementById("detailpanel");
-
-  detailsPanel.classList.remove("details-on");
-  detailsPanel.classList.add("details-off");
-
-}
-
-function openDetailPanel() {
-  // Function to Close the details panel:
-
-  const detailsPanel = document.getElementById("detailpanel");
-
-  detailsPanel.classList.remove("details-off");
-  detailsPanel.classList.add("details-on");
-
-}
-
-const closeDetailButton = document.getElementById("closedetailbutton");
-closeDetailButton.addEventListener("click", closeDetailPanel);
 
 // Adding Dragging to Elements
 addDraggingToElements();
