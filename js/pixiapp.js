95084b48a6b3203437d6891c7f01e178fce823fd
Mubdi Rahman <mubdi@sidratresearch.com>
2024-05-14 22:38:01 -0400

    Testing Pixi

--- js/pixiapp.js
+++ js/pixiapp.js
@@ -15,6 +15,15 @@ PIXI.Sprite.prototype.bringToFront = function () {
     }
 }
 
+// Generate Random Numbers for testing
+
+function generateArrayRandom(n_array) {
+    let random_array = Array.from({ length: n_array }, () => Math.random())
+    return random_array
+}
+
+const tmp_x = generateArrayRandom(80000);
+const tmp_y = generateArrayRandom(80000);
 
 // Grabbing Main Container
 const main_container = document.getElementById("app");
@@ -27,6 +36,53 @@ const getAppWidth = function () {
 
 const getAppHeight = function () {
     return main_container.clientHeight;
+}
+
+// Function for making template shape
+
+function makePixiTemplate(app) {
+    // Template Shape
+
+    const templateShape = new PIXI.Graphics()
+        .fill(0xffffff)
+        .setStrokeStyle({ width: 1, color: 0x333333, alignment: 0 })
+        .circle(0, 0, 8 * POINTRADIUS);
+
+
+    const { point_width, point_height } = templateShape;
+
+    // Draw the circle to the RenderTexture
+    const renderTexture = PIXI.RenderTexture.create({
+        point_width,
+        point_height,
+        multisample: PIXI.MSAA_QUALITY.HIGH,
+        resolution: window.devicePixelRatio
+    });
+
+    const point_container = new PIXI.Container()
+
+    const plot_layer = new PIXI.Graphics()
+    plot_layer.circle(0, 0, POINTRADIUS);
+    plot_layer.fill(0xcccccc);
+
+
+    const texture = app.renderer.generateTexture(plot_layer);
+
+    return texture;
+}
+
+
+// Functions for range
+
+function getRangeWithBorder(data_array) {
+    let fullExtent = d3.extent(data_array);
+
+    let initialRange = fullExtent[1] - fullExtent[0];
+
+    fullExtent[0] = fullExtent[0] - (DATABORDERBUFFER * initialRange);
+    fullExtent[1] = fullExtent[1] + (DATABORDERBUFFER * initialRange);
+
+    return fullExtent
 }
 
 
@@ -64,5 +120,22 @@ export async function initializePixiApp() {
 
     // Putting Pixi Scene into Container
     main_container.appendChild(app.canvas);
+
+    let xRange = getRangeWithBorder(tmp_x);
+    let yRange = getRangeWithBorder(tmp_y).reverse();
+
+    // Setting up Scalers 
+
+    const x_scaler = d3.scaleLinear()
+        .domain(xRange)
+        .range([0, WIDTH]);
+
+    const y_scaler = d3.scaleLinear()
+        .domain(yRange)
+        .range([0, HEIGHT]);
+
+
+
+    const texture = makePixiTemplate(app);
 
 }
