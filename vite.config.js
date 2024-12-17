39b4341ccbcb9c934d85075c562bcc53ee6a0b95
Mubdi Rahman <mubdi@sidratresearch.com>
2024-12-17 18:50:39 -0500

    Start of new page

--- vite.config.js
+++ vite.config.js
@@ -27,6 +27,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+        app: resolve(__dirname, "app.html"),
         details: resolve(__dirname, "details.html"),
       },
     },
